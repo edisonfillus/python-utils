1ca192bff34f3c91b4d09cbd66e83573cac23f88
Edison Fillus <efillus@tripadvisor.com>
2021-11-20 21:21:22 +0000

    Included prime_efficient.py

---
+++ src/prime_efficient.py
@@ -0,0 +1,17 @@
+def primality(n):
+    if (n < 2):
+      return false
+    elif (n == 2):
+      return true
+    elif (n % 2 == 0):
+      return false
+
+    sqrt = int(math.sqrt(n))
+    for i in range(3, sqrt, 2):
+      if (n % i == 0):
+        return false
+
+    return true
+
+
+primality(75)
