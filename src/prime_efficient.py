744b441ee97abb611e0160f46b6983577dde3133
Edison Fillus <efillus@tripadvisor.com>
2021-11-20 21:23:39 +0000

    Fix one edge case on prime_efficient.py

--- src/prime_efficient.py
+++ src/prime_efficient.py
@@ -6,7 +6,7 @@ def primality(n):
     elif (n % 2 == 0):
       return false
 
-    sqrt = int(math.sqrt(n))
+    sqrt = int(math.sqrt(n)+1)
     for i in range(3, sqrt, 2):
       if (n % i == 0):
         return false
